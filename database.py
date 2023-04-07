13cf538284382e29f17fad21a364596bc727c53b
Pankaj Patel <86610118+pankaj1980patel@users.noreply.github.com>
2023-04-07 08:15:10 +0000

    database url modified

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-dbConnectionString = "mysql+pymysql://z90snti9zhc76auts0p1:pscale_pw_aWeY7W64cJqo3nQ0g0FaNl83DlYgWRRb2Lo9fs9A5xL@aws.connect.psdb.cloud/panther-carrers-database?charset=utf8mb4"
+dbConnectionString = "mysql+pymysql://b7nrd9huv9u1kb0iguc5:pscale_pw_9GUkGWwkAMqf4BBYHhHdhk4EXj9ismaMSnZBbKCHInu@aws.connect.psdb.cloud/panther-carrers-database?charset=utf8mb4"
 
 engine = create_engine(dbConnectionString,
     connect_args={
@@ -17,3 +17,4 @@ def loadJobs():
     for job in result.all():
       jobs.append(dict(job._mapping))
     return jobs
+print(loadJobs())
