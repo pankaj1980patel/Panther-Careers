0ad2c4d23c1dd91b369e667cb0753122acdebee9
Pankaj Patel <86610118+pankaj1980patel@users.noreply.github.com>
2023-04-10 09:04:51 +0000

    rollback to without database mode

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
 from flask import Flask, render_template, jsonify
-from database import loadJobs
 
 app = Flask(__name__)
 
-# JOBS = [
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-# ]
-
+JOBS = [
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+]
 
 
 @app.route('/')
 def helloWorld():
-  JOBS = loadJobs()
   return render_template('home.html', jobs=JOBS)
 
 
 @app.route('/api/jobs')
 def apiresponse():
-  JOBS = loadJobs()
   return jsonify(JOBS)
 
 
