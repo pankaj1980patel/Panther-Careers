1aa4bcbf3d0c4d91de8df8f72fee5364445be3f2
Pankaj Patel <86610118+pankaj1980patel@users.noreply.github.com>
2023-04-10 12:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,50 +3,50 @@ from database import loadJobs
 
 app = Flask(__name__)
 
-# JOBS = [
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-#   {
-#     'id': 1,
-#     'title': 'some',
-#     'location': 'new york',
-#     'salary': 'Rs. 1200000'
-#   },
-# ]
+JOBS = [
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+  {
+    'id': 1,
+    'title': 'some',
+    'location': 'new york',
+    'salary': 'Rs. 1200000'
+  },
+]
 
 
 
 @app.route('/')
 def helloWorld():
-  JOBS = loadJobs()
+#   JOBS = loadJobs()
   return render_template('home.html', jobs=JOBS)
 
 
 @app.route('/api/jobs')
 def apiresponse():
-  JOBS = loadJobs()
+#   JOBS = loadJobs()
   return jsonify(JOBS)
 
 
